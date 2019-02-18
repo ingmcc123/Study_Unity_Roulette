6c46c89fc3c85f5facf0d1c8b02b8550a43ea874
Minchang Cheon <43161094+ingmcc123@users.noreply.github.com>
2019-02-19 03:34:14 +0900

    Add files via upload

---
+++ Assets/RouletteController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RouletteController : MonoBehaviour {
+
+    float rotSpeed = 0;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        if(Input.GetMouseButtonDown(0)) {
+            this.rotSpeed = 100;
+        }
+
+        transform.Rotate(0, 0, this.rotSpeed);
+
+        this.rotSpeed *= 0.99f;
+	}
+}
